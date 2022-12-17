6487fd74b8a76ddccd662441d8bf1dfc49d8c4d9
akane0151 <akane0151@yahoo.com>
2022-12-17 12:19:48 +0330

    app name changed!

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>سرویس Eragon</title>
+    <title>Dornika app</title>
     <!-- Bootstrap -->
     <link href="{{asset('vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{asset('vendors/bootstrap-rtl/dist/css/bootstrap-rtl.min.css')}}" rel="stylesheet">
