b71eb09700264f6e012bad54cd45010941e7d5a1
akane0151 <akane0151@yahoo.com>
2023-01-09 11:50:52 +0330

    admin panel role and static permissions added
    view, edit, change password for users.
    view, edit, remove states and cities.
    view, add admin users.

--- resources/views/admin/layout/topbar.blade.php
+++ resources/views/admin/layout/topbar.blade.php
@@ -14,38 +14,14 @@
                         <span class=" fa fa-angle-down"></span>
                     </a>
                     <ul class="dropdown-menu dropdown-usermenu pull-right">
-{{--                        <li>--}}
-{{--                            <a href="/admin/settings">--}}
-{{--                                <span>تنظیمات</span>--}}
-{{--                            </a>--}}
-{{--                        </li>--}}
                         <li><a href="/admin/changePassword">تغییر رمزعبور</a></li>
                         <li><a href="/admin/logout"><i class="fa fa-sign-out pull-right"></i> خروج</a></li>
                     </ul>
                 </li>
-
-                <li role="presentation" class="dropdown">
-                    <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown"
-                       aria-expanded="false">
-                        <i class="fa fa-envelope-o"></i>
-                        <span class="badge bg-green">0</span>
-                    </a>
-                    <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
-{{--                        <li>--}}
-{{--                            <a>--}}
-{{--                                <span class="image"><img src="{{asset('build/images/img.jpg')}}" alt="Profile Image"/></span>--}}
-{{--                                <span>--}}
-{{--                          <span>مرتضی کریمی</span>--}}
-{{--                          <span class="time">3 دقیقه پیش</span>--}}
-{{--                        </span>--}}
-{{--                                <span class="message">--}}
-{{--                          فیلمای فستیوال فیلمایی که اجرا شده یا راجع به لحظات مرده ایه که فیلمسازا میسازن. آنها جایی بودند که....--}}
-{{--                        </span>--}}
-{{--                            </a>--}}
-{{--                        </li>--}}
-
-                    </ul>
+                <li>
+                    <a href="/">صفحه اصلی</a>
                 </li>
+
             </ul>
         </nav>
     </div>
