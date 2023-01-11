8bcb719cb7de2269b266bc003cfa4f269c95bd35
akane0151 <akane0151@yahoo.com>
2023-01-11 16:25:00 +0330

    Helper functions added. read time estimate function added.

--- app/Helpers/HelperFuncs.php
+++ app/Helpers/HelperFuncs.php
@@ -10,12 +10,12 @@ namespace App\Helpers;
      */
     function readTime(string $text)
     {
-//        if (empty($text)) {
-//            return 0;
-//        }
+        if (empty($text)) {
+            return 0;
+        }
         setlocale(LC_ALL, 'Farsi');
         $textOnly = strip_tags($text);
-        $wordsCount = str_word_count($textOnly);
+        $wordsCount = str_word_count($textOnly, 0 , 'ابپتثجچ‌حخدذرز‌ژس‌شصضطظعغفقکگلمنوهیءآاًهٔه');
         $wordsPerMin = 200;
 
         return ceil($wordsCount / $wordsPerMin);
